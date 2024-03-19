5802b410e7ae14fdcbe2534055ef7396a14419c5
Сергей Полевой <serg.021@yandex.ru>
2024-03-19 20:01:42 +0300

    add HW-05

---
+++ HW-05/Task#1/Client_Manager.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include <pqxx/pqxx>
+
+
+class Manager {
+private:
+	std::string connection;
+	std::string name;
+	std::string last_name;
+	std::string email;
+	std::string phone_number;
+public:
+
+};
