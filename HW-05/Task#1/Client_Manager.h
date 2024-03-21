3895b246fc16d736130e7d5f537bb9f46b2f7cf1
ZeRg0912 <serg.021@yandex.ru>
2024-03-21 15:40:34 +0300

    draft sql pqxx

--- HW-05/Task#1/Client_Manager.h
+++ HW-05/Task#1/Client_Manager.h
@@ -1,11 +1,12 @@
 #pragma once
 #include <iostream>
 #include <vector>
+#include <string>
 #include <pqxx/pqxx>
 
 struct Client {
-	std::string connect;
-	std::string name;
+	int id;
+	std::string first_name;
 	std::string last_name;
 	std::string email;
 	std::vector <std::string> phone_number;
@@ -13,13 +14,63 @@ struct Client {
 
 class Manager {
 private:
-	std::string conn;
+	std::string conn_string;
+	pqxx::connection conn;
+
+	pqxx::result ExecuteQuery(
+		const std::string& query, 
+		const std::vector <std::string>& parameters
+	);
+
+	void ExecuteUpdate(
+		const std::string& query,
+		const std::vector<std::string>& parameters
+	);
+
 public:
+	Manager(
+		const std::string& dbname,
+		const std::string& user,
+		const std::string& password,
+		const std::string& host,
+		const std::string& port
+	);
+
 	void CreateTable();
-	void CreateClient();
-	void AddPhoneNumber();
-	void ChangeClient();
-	void DeletePhoneNumber();
-	void DeleteClient();
-	void ClientInfo();
+
+	void AddClient(
+		const std::string& first_name,
+		const std::string& last_name,
+		const std::string& email
+	);
+
+	void AddPhone(
+		const std::string& first_name,
+		const std::string& last_name,
+		const std::string& phone
+	);
+
+	void UpdateClient(
+		const std::string& first_name,
+		const std::string& last_name,
+		const std::string& email,
+
+		const std::string& new_first_name,
+		const std::string& new_last_name,
+		const std::string& new_email
+	);
+
+	void DeletePhone(
+		const std::string& first_name,
+		const std::string& last_name,
+		const std::string& phone
+	);
+
+	void DeleteClient(
+		const std::string& first_name,
+		const std::string& last_name,
+		const std::string& email
+	);
+
+	std::vector<Client> FindClient(const std::string& search_term);
 };
